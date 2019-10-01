58664c490e46a6a2aee232c019600ba2dad39090
Patric Kanngiesser <patric.kanngiesser@jtl-software.com>
2019-10-01 17:54:49 +0200

    WIP

--- src/FooListener.php
+++ src/FooListener.php
@@ -10,12 +10,24 @@
 namespace JTL\SCX\Channel;
 
 use JTL\Nachricht\Contract\Listener\Listener;
+use JTL\SCX\Lib\Channel\Event\Seller\OrderShippingEvent;
 use JTL\SCX\Lib\Channel\Event\Seller\SystemNotificationEvent;
+use JTL\SCX\Lib\Channel\Event\Seller\SystemTestEvent;
 
 class FooListener implements Listener
 {
     public function test(SystemNotificationEvent $event): void
     {
-        var_dump($event);
+        echo "NOTIFICATION\n";
+    }
+
+    public function shipping(OrderShippingEvent $event): void
+    {
+        echo "SHIPPING\n";
+    }
+
+    public function testEvent(SystemTestEvent $event): void
+    {
+        echo "TEST\n";
     }
 }
