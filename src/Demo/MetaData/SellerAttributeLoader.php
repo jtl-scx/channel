5d03f7379fdd9018b4d55d922f86aa07a6eabac5
Ronny Herrgesell <ronny.herrgesell@jtl-software.com>
2020-03-04 14:29:57 +0000

    Fix autoloading

---
+++ src/Demo/MetaData/SellerAttributeLoader.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+/**
+ * This File is part of JTL-Software
+ *
+ * User: rherrgesell
+ * Date: 3/4/20
+ */
+
+namespace JTL\SCX\Channel\Demo\MetaData;
+
+
+use JTL\SCX\Lib\Channel\Contract\MetaData\SellerAttributeLoader as SellerAttributeLoaderAlias;
+use JTL\SCX\Lib\Channel\MetaData\Attribute\AttributeList;
+
+class SellerAttributeLoader implements SellerAttributeLoaderAlias
+{
+    public function fetchAll(string $sellerId): AttributeList
+    {
+        return new AttributeList();
+    }
+}
